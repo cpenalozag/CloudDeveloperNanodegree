08f4d0d2f0cca57122cea03887ce9e070baecb68
Carlos Penaloza <c.penaloza@uniandes.edu.co>
2020-04-10 22:11:54 -0500

    Project done

---
+++ course-04/project/backend/src/lambda/http/updateTodo.ts
@@ -0,0 +1,25 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+
+import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import { updateTodo } from '../../businessLogic/todos'
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const todoId = event.pathParameters.todoId
+  const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+
+  const todo = await updateTodo(todoId, updatedTodo)
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      todo
+    })
+  }
+}
---
+++ course-04/project/client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '0jyw0cfm70'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'cloud-developer-nanodegree.auth0.com',
+  clientId: 'nFvulVz5TOOSMT3JTYIJkQhHTM50rzB4',
+  callbackUrl: 'http://localhost:3000/callback'
+}
